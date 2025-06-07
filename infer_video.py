9493ee9e8d7c424e989a8b1363aada471bcea43f
CurtaTudor <curta.tudor@gmail.com>
2025-06-07 10:13:16 +0300

    Removed fps counter from video inference

--- infer_video.py
+++ infer_video.py
@@ -1,5 +1,6 @@
 # python infer_video.py --input input/inference_data/videos/DJI.mp4 --device cpu --imgsz 1568 1088
 # python infer_video.py --input input/inference_data/videos/my_video.mp4 --imgsz 1568 1088
+# python infer_video.py --input input/inference_data/videos/video_7.mp4 --device cpu
 
 from transformers import (
     SegformerFeatureExtractor, 
@@ -41,9 +42,6 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-out_dir = 'out/outputs/inference_results_video'
-os.makedirs(out_dir, exist_ok=True)
-
 extractor = SegformerFeatureExtractor()
 model = SegformerForSemanticSegmentation.from_pretrained(args.model)
 model.to(args.device).eval()
@@ -57,46 +55,22 @@ else:
     frame_height = args.imgsz[1]
 vid_fps = int(cap.get(5))
 save_name = args.input.split(os.path.sep)[-1].split('.')[0]
-# Define codec and create VideoWriter object.
-out = cv2.VideoWriter(f"{out_dir}/{save_name}.mp4", 
-                    cv2.VideoWriter_fourcc(*'mp4v'), 30, 
-                    (frame_width, frame_height))
-
-frame_count = 0
-total_fps = 0
 while cap.isOpened:
     ret, frame = cap.read()
     if ret:
-        frame_count += 1
         image = frame
         if args.imgsz is not None:
             image = cv2.resize(image, (args.imgsz[0], args.imgsz[1]))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
         # Get labels.
-        start_time = time.time()
         labels = predict(model, extractor, image, args.device)
-        end_time = time.time()
-
-        fps = 1 / (end_time - start_time)
-        total_fps += fps
         
         # Get segmentation map.
         seg_map = draw_segmentation_map(
             labels.cpu(), LABEL_COLORS_LIST
         )
         outputs = image_overlay(image, seg_map)
-        cv2.putText(
-            outputs,
-            f"{fps:.1f} FPS",
-            (15, 35),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=1,
-            color=(0, 0, 255),
-            thickness=2,
-            lineType=cv2.LINE_AA
-        )
-        out.write(outputs)
         cv2.imshow('Image', outputs)
         # Press `q` to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -108,7 +82,3 @@ while cap.isOpened:
 cap.release()
 # Close all frames and video windows.
 cv2.destroyAllWindows()
-
-# Calculate and print the average FPS.
-avg_fps = total_fps / frame_count
-print(f"Average FPS: {avg_fps:.3f}")
